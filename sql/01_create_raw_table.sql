873ac7304e59415f43b27a5be21719a0076b4330
Suvekshya_Neupane <sneup3@unh.newhaven.edu>
2026-01-18 18:34:48 -0500

    Add raw table creation script

---
+++ sql/01_create_raw_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dbo.OnlineRetail_Raw (
+    InvoiceNo     NVARCHAR(50),
+    StockCode     NVARCHAR(50),
+    Description   NVARCHAR(255),
+    Quantity      NVARCHAR(50),
+    InvoiceDate   NVARCHAR(50),
+    UnitPrice     NVARCHAR(50),
+    CustomerID    NVARCHAR(50),
+    Country       NVARCHAR(100)
+)
